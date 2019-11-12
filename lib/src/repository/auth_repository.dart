108a11c01373f75f31f0661a09e007e48dab8b0a
rodrigo <cantelli.r@gmail.com>
2019-11-11 22:56:55 -0300

    refactor to repository pattern

--- lib/src/repository/auth_repository.dart
+++ lib/src/repository/auth_repository.dart
@@ -1,8 +1,10 @@
-abstract class AuthService {
+abstract class AuthRepository {
   Future<void> login(String email, String password);
   Future<void> loginByGoogle();
   Future<void> loginByFacebook();
   Future<void> logout();
   Future<void> register(String email, String password);
   Future<void> recoverPassword(String email);
+  Future<bool> isLoggedIn();
+  Future<String> getEmail();
 }
